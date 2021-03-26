54f91ae79c2d6f5810e781737c1e3491957ba297
Ayça Yıldırım <eaycayildirim@gmail.com>
2021-03-26 12:58:48 +0300

    Service lane problem: C# solution

---
+++ Problem Solving/Algorithms/Implementation/ServiceLane/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ServiceLane
+{
+    class Program
+    {
+        static int[] serviceLane(int n, int[] width, int[][] cases)
+        {
+            int[] result = new int[cases.Length];
+
+            for (int i = 0; i < cases.Length; i++)
+            {
+                int min = 100;
+                for (int j = cases[i][0]; j <= cases[i][1]; j++)
+                {
+                    if (min > width[j])
+                        min = width[j];
+                }
+                result[i] = min;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 8;
+            int[] width = { 2, 3, 1, 2, 3, 2, 3, 3 };
+            int[][] cases =
+            {
+                new int[] { 0, 3 },
+                new int[] { 4, 6 },
+                new int[] { 6, 7 },
+                new int[] { 3, 5 },
+                new int[] { 0, 7 }
+            };
+
+            Array.ForEach(serviceLane(n, width, cases), Console.WriteLine);
+        }
+    }
+}
