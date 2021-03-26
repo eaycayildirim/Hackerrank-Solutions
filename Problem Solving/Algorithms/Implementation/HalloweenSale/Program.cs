e72b771237edcb459e3e2778ec62f7d5ec8a2d69
Ayça Yıldırım <eaycayildirim@gmail.com>
2021-03-26 12:48:56 +0300

    Halloween sale problem: C# solution

---
+++ Problem Solving/Algorithms/Implementation/HalloweenSale/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HalloweenSale
+{
+    class Program
+    {
+        static int howManyGames(int p, int d, int m, int s)
+        {
+            int count = 0;
+            while (!(s < m) && !(s < p))
+            {
+                if (p - d <= m)
+                {
+                    s -= m;
+                    count++;
+                }
+                else
+                {
+                    s -= p;
+                    p -= d;
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            int price = 20;
+            int discount = 3;
+            int minimum = 6;
+            int startingBudget = 80;
+
+            Console.WriteLine(howManyGames(price, discount, minimum, startingBudget));
+        }
+    }
+}
