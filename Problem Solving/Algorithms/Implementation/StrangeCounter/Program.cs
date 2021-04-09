9dd4d049e6c8801ac91ca56d53c2bae641ecb309
Ayça Yıldırım <eaycayildirim@gmail.com>
2021-04-09 15:49:40 +0300

    Strange counter problem: C# solution

---
+++ Problem Solving/Algorithms/Implementation/StrangeCounter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StrangeCounter
+{
+    class Program
+    {
+        static long strangeCounter(long t)
+        {
+            long count = 4;
+
+            while (count <= t)
+            {
+                count = count * 2 + 2;
+            }
+            return count - t;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(strangeCounter(4));
+        }
+    }
+}
