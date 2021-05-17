b1748245982c5355c822ac9d2c069d68c56cd84a
Ayça Yıldırım <eaycayildirim@gmail.com>
2021-05-17 15:41:15 +0300

    Weighted uniform strings problem: C# solution

---
+++ Problem Solving/Algorithms/Strings/Weighted Uniform Strings/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WeightedUniformStrings
+{
+    class Program
+    {
+        public static string[] weightedUniformStrings(string s, int[] queries)
+        {
+            List<string> list = new List<string>();
+            int count = 1;
+            HashSet<int> set = new HashSet<int>();
+            set.Add((int)s[0] - 96);
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i - 1] == s[i])
+                {
+                    count++;
+                    set.Add(((int)s[i] - 96) * count);
+                }
+                else
+                {
+                    set.Add((int)s[i] - 96);
+                    count = 1;
+                }
+            }
+
+            for (int i = 0; i < queries.Length; i++)
+            {
+                if (set.Contains(queries[i]))
+                    list.Add("Yes");
+                else
+                    list.Add("No");
+            }
+            return list.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            string s = "abccddde";
+
+            int[] queries = { 1, 3, 12, 5, 9, 10 };
+
+            Array.ForEach(Program.weightedUniformStrings(s, queries), Console.WriteLine);
+        }
+    }
+}
