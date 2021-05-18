8a0ee0d96703f845d2b1b11904f4726bb78f7c69
Ayça Yıldırım <eaycayildirim@gmail.com>
2021-05-18 15:15:00 +0300

    commit

---
+++ Problem Solving/Data Structures/Linked Lists/linked/linked/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace linked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
