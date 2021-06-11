bb774b5a841e8e66fb311a752429e990867b5e4c
Ayça Yıldırım <eaycayildirim@gmail.com>
2021-06-11 12:16:22 +0300

    Feat: Add insert a node at a spesific position

---
+++ Problem Solving/Data Structures/Linked Lists/Insert a Node at a Specific Position/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace InsertANodeAtASpesificPosition
+{
+    class Solution
+    {
+
+        class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData)
+            {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+
+        class SinglyLinkedList
+        {
+            public SinglyLinkedListNode head;
+            public SinglyLinkedListNode tail;
+
+            public SinglyLinkedList()
+            {
+                this.head = null;
+                this.tail = null;
+            }
+
+            public void InsertNode(int nodeData)
+            {
+                SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+                if (this.head == null)
+                {
+                    this.head = node;
+                }
+                else
+                {
+                    this.tail.next = node;
+                }
+
+                this.tail = node;
+            }
+        }
+
+        static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+        {
+            while (node != null)
+            {
+                textWriter.Write(node.data);
+
+                node = node.next;
+
+                if (node != null)
+                {
+                    textWriter.Write(sep);
+                }
+            }
+        }
+
+        static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode llist, int data, int position)
+        {
+            SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+
+            SinglyLinkedListNode temp = llist;
+            int count = 0;
+            if (llist.next == null)
+                return newNode;
+            if(position == 0)
+            {
+                newNode.next = llist;
+                return newNode;
+            }
+                
+            while (count != position-1)
+            {
+                count++;
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+            return llist;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@"C:\Users\Sabreden Derviş\Masaüstü");
+
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                llist.InsertNode(llistItem);
+            }
+
+            int data = Convert.ToInt32(Console.ReadLine());
+
+            int position = Convert.ToInt32(Console.ReadLine());
+
+            SinglyLinkedListNode llist_head = insertNodeAtPosition(llist.head, data, position);
+
+            PrintSinglyLinkedList(llist_head, " ", textWriter);
+            textWriter.WriteLine();
+        }
+    }
+}
+
---
+++ Problem Solving/Data Structures/Linked Lists/Insert a Node at a Specific Position/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Insert a Node at a Specific Position")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Insert a Node at a Specific Position")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("afa9c3db-c7b7-475d-99ce-68fa6cd4d73c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
